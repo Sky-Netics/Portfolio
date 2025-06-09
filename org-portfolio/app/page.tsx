d389d568cf2788df97656915215ce44b19747b4c
amirhosseinzakeri715 <amirhosseinzakeri715@gmail.com>
2025-06-09 11:32:09 +0330

    adding sticky parts

--- org-portfolio/app/page.tsx
+++ org-portfolio/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Sticky2 from "./components/Sticky2";
+import Sticky from "./components/Sticky";
+
 
 export default function Home() {
   return (
     
     <div>
       <Hero/>
+      <Sticky/>
+      <Sticky2/>
     </div>
   );
 }
