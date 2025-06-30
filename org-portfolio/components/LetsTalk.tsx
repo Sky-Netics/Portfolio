662af57e9ceae0ce7caf7cfd1d60186295946de6
amirhosseinzakeri715 <amirhosseinzakeri715@gmail.com>
2025-06-30 11:37:21 +0330

    refactoring letstalk

--- org-portfolio/components/LetsTalk.tsx
+++ org-portfolio/components/LetsTalk.tsx
@@ -8,7 +8,7 @@ const LetsTalkSection = () => {
     <section className="relative flex max-h-screen items-center justify-center bg-white overflow-hidden pb-15 ">
       <div className="container relative z-10 mx-auto px-6 py-20 text-center">
         <h1 className="mb-6 font-heading text-6xl font-bold leading-tight text-gray-900 md:text-7xl lg:text-8xl xl:text-9xl">
-          let<span className="text-primary">'</span>s talk!
+          Let<span className="text-primary">&apos;</span>s Talk!
         </h1>
 
         <h2 className="mb-8 text-xl font-semibold text-gray-800/90 md:text-2xl lg:mb-12 lg:text-3xl">
@@ -22,10 +22,8 @@ const LetsTalkSection = () => {
 
         <div className="flex items-center justify-center">
           <Link href="/contact" legacyBehavior>
-            <a>
-              <button className=" inline-flex h-12 max-w-fit items-center justify-center rounded-xl border-2 border-slate-500 bg-[linear-gradient(110deg,#FFFFFF,45%,#e9eaea,55%,#FFFFFF)] bg-[length:200%_100%] px-6 font-medium text-black   focus:ring-2 focus:ring-slate-50 focus:ring-offset-1 focus:ring-offset-slate-50 md:text-lg hover:scale-105 cursor-pointer transition-all duration-500 lg:text-xl mt-6 mb-16">
-                Contact Us
-              </button>
+            <a className="inline-flex h-12 max-w-fit items-center justify-center rounded-xl border-2 border-slate-500 bg-[linear-gradient(110deg,#FFFFFF,45%,#e9eaea,55%,#FFFFFF)] bg-[length:200%_100%] px-6 font-medium text-black focus:ring-2 focus:ring-slate-50 focus:ring-offset-1 focus:ring-offset-slate-50 md:text-lg hover:scale-105 cursor-pointer transition-all duration-500 lg:text-xl mt-6 mb-16">
+              Contact Us
             </a>
           </Link>
         </div>
