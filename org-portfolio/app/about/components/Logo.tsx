75f579942489282d5ff1374acd01bc309e56f292
amirhosseinzakeri715 <amirhosseinzakeri715@gmail.com>
2025-06-30 12:00:54 +0330

    refactoring /about logo

--- org-portfolio/app/about/components/Logo.tsx
+++ org-portfolio/app/about/components/Logo.tsx
@@ -3,24 +3,30 @@ import Image from "next/image";
 import logoPng from "../../../public/images/logo-transparent-png.png";
 import logoPng2 from '../../../public/images/logo-transparent-png2.png';
 
+const logos = [
+  {
+    src: logoPng,
+    alt: "Sky Netics Logo - Primary",
+  },
+  {
+    src: logoPng2,
+    alt: "Sky Netics Logo - Secondary",
+  },
+];
+
 const Logo = () => {
   return (
-    <div className="grid md:grid-cols-2 max-w-4xl mx-auto my-15 gap-8 p-4">
-      <Image
-        src={logoPng}
-        alt="sky netics Logo"
-        width={900}
-        height={900}
-        className="  h-auto object-contain"
-      />
-      <Image
-        src={logoPng2}
-        alt="sky netics Logo"
-        width={900}
-        height={900}
-        className="  h-auto object-contain"
-      />
-
+    <div className="grid md:grid-cols-2 max-w-4xl mx-auto my-16 gap-8 p-4 py-16">
+      {logos.map((logo) => (
+        <Image
+          key={logo.alt}
+          src={logo.src}
+          alt={logo.alt}
+          width={900}
+          height={900}
+          className="h-auto object-contain"
+        />
+      ))}
     </div>
   );
 };
